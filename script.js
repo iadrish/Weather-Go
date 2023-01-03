d6fbf63a11bb70e22f06fbb5647fe586bddb6a16
Adrish Bhattacharya <88700100+iadrish@users.noreply.github.com>
2023-01-03 09:30:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,17 +6,20 @@ const options = {
     }
 };
 const getWeather = (city) => {
-    cityName.innerHTML=city
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city'+city, options)
+    cityName.innerHTML = city
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
         .then(response => response.json())
         .then((response) => {
             cloud_pct.innerHTML = response.cloud_pct
             temp.innerHTML = response.temp
+            temp_2.innerHTML = response.temp
             feels_like.innerHTML = response.feels_like
             humidity.innerHTML = response.humidity
+            humidity2.innerHTML = response.humidity
             min_temp.innerHTML = response.min_temp
             max_temp.innerHTML = response.max_temp
             wind_speed.innerHTML = response.wind_speed
+            wind_speed2.innerHTML = response.wind_speed
             wind_degrees.innerHTML = response.wind_degrees
             sunrise.innerHTML = response.sunrise
             sunset.innerHTML = response.sunset
